e193dd22da6fc015201d61d3e4d6c7d4b79795db
Stanley <stanley@Stanleys-MacBook-Air.local>
2022-03-23 15:02:33 +0100

    update server

---
+++ app/models/user.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+const { isNumberObject } = require("util/types");
+
+const User = mongoose.model(
+  'User', 
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+      min: 20
+  },
+  phoneNumber: {
+      type: Number,
+      required: true,
+      min: 3,
+      max: 20
+  },
+  nationalIdentityNumber: {
+      type: Number,
+      required: true,
+      min: 11,
+      max: 11,
+      unique: true
+  },
+  location: {
+      type: String,
+      required: true,
+      max: 50
+  },
+  password: {
+      type: String,
+      required: true,
+      min: 8,
+      unique: true
+  }
+  }, {timestamps: true})
+);
