e4ba9e8b866bae619894477d1973ba2845a415aa
Gbemisola Kotoye <barkty@yahoo.com>
2022-03-25 08:33:28 +0100

    hot fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 let corsOptions = {
     //origin: 'http://127.0.0.1:5501',
-    origin: 'https://frontend-chowbetter.vercel.app/',
+    origin: 'https://frontend-chowbetter.vercel.app',
     methods: 'GET, HEAD, PUT, PATCH, POST, OPTIONS, DELETE',
     credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization'],
